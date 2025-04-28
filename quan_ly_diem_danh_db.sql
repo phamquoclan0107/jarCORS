e0fbd5681466c6e1570ea4d3c00f8b0108232d98
PhamQuocLan <hao12398700@gmail.com>
2025-04-28 10:10:02 +0700

    fix_final3

--- quan_ly_diem_danh_db.sql
+++ quan_ly_diem_danh_db.sql
@@ -28,21 +28,6 @@ SET time_zone = "+00:00";
 --
 
 
-CREATE TABLE IF NOT EXISTS `buoi_hoc` (
-  `ma_buoi_hoc` int NOT NULL AUTO_INCREMENT,
-  `ngay_hoc` date NOT NULL,
-  `tiet_bat_dau` int NOT NULL,
-  `tiet_ket_thuc` int NOT NULL,
-  `ma_mon_hoc` varchar(255) NOT NULL,
-  `ma_phong` varchar(255) NOT NULL,
-  `ma_tham_gia` varchar(255) DEFAULT NULL,
-  `gv_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`ma_buoi_hoc`),
-  KEY `FKresnpp25ibokky9xcp7r9nn1e` (`ma_mon_hoc`),
-  KEY `FK1xu8i7qikw0dq270q1ejw8n27` (`ma_phong`),
-  CONSTRAINT `FKresnpp25ibokky9xcp7r9nn1e` FOREIGN KEY (`ma_mon_hoc`) REFERENCES `mon_hoc` (`ma_mon_hoc`),
-  CONSTRAINT `FK1xu8i7qikw0dq270q1ejw8n27` FOREIGN KEY (`ma_phong`) REFERENCES `phong` (`ma_phong`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
 --
@@ -57,20 +42,7 @@ CREATE TABLE IF NOT EXISTS `buoi_hoc` (
 --
 
 
-CREATE TABLE IF NOT EXISTS `diem_danh` (
-  `ma_buoi_hoc` int NOT NULL,
-  `ma_diem_danh` int NOT NULL AUTO_INCREMENT,
-  `thoi_gian_diem_danh` datetime(6) DEFAULT NULL,
-  `ma_nguoi_dung` varchar(255) NOT NULL,
-  `phuong_thuc` enum('manual','qr_code') NOT NULL,
-  `trang_thai` enum('absent','late','present') NOT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `created_at` datetime(6) DEFAULT NULL,
-  `expired_at` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`ma_diem_danh`),
-  KEY `FKn39tnia8bkst1qbvkosdx8lew` (`ma_buoi_hoc`),
-  KEY `FK56ahhb3iy1e50gim96jbaxyku` (`ma_nguoi_dung`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 
 --
 -- Dumping data for table `diem_danh`
@@ -84,16 +56,6 @@ CREATE TABLE IF NOT EXISTS `diem_danh` (
 --
 
 
-CREATE TABLE IF NOT EXISTS `diem_danh_sinh_vien` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `time` datetime(6) DEFAULT NULL,
-  `diemdanh_id` int DEFAULT NULL,
-  `user_id` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKtflyllv7mahy2d1wxf18dncf9` (`diemdanh_id`),
-  KEY `FK5expuo5jtc2lkdw7n6wvngej8` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `diem_danh_sinh_vien`
@@ -106,6 +68,21 @@ CREATE TABLE IF NOT EXISTS `diem_danh_sinh_vien` (
 --
 -- Table structure for table `lop_sinh_vien`
 --
+CREATE TABLE IF NOT EXISTS `tai_khoan` (
+  `ma_tai_khoan` varchar(255) NOT NULL,
+  `mat_khau` varchar(255) NOT NULL,
+  `ten_tai_khoan` varchar(255) NOT NULL,
+  `role` enum('admin','student','teacher') NOT NULL,
+  PRIMARY KEY (`ma_tai_khoan`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tai_khoan`
+--
+
+INSERT INTO `tai_khoan` (`ma_tai_khoan`, `mat_khau`, `ten_tai_khoan`, `role`) VALUES
+('1234', '$2a$10$X.lhuj27aTwEUo9kNiokxexwIEjoVzaUkKD.RROLuSHcqDbuy6IHq', 'nguyenvana', 'admin')
+
 
 
 CREATE TABLE IF NOT EXISTS `lop_sinh_vien` (
@@ -126,78 +103,6 @@ INSERT INTO `lop_sinh_vien` (`ma_lop`, `ten_lop`) VALUES
 --
 -- Table structure for table `mon_hoc`
 --
-
-
-CREATE TABLE IF NOT EXISTS `mon_hoc` (
-  `ma_mon_hoc` varchar(255) NOT NULL,
-  `ten_mon_hoc` varchar(255) NOT NULL,
-  PRIMARY KEY (`ma_mon_hoc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `mon_hoc`
---
-
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `nguoi_dung`
---
-
-
-CREATE TABLE IF NOT EXISTS `nguoi_dung` (
-  `email` varchar(255) DEFAULT NULL,
-  `ma_lop` varchar(255) DEFAULT NULL,
-  `ma_nguoi_dung` varchar(255) NOT NULL,
-  `ma_tai_khoan` varchar(255) DEFAULT NULL,
-  `sdt` varchar(255) DEFAULT NULL,
-  `ten_nguoi_dung` varchar(255) NOT NULL,
-  PRIMARY KEY (`ma_nguoi_dung`),
-  UNIQUE KEY `UKmajqh5g4djy2tp3p9dvr64brp` (`email`),
-  UNIQUE KEY `UKltruy9naf2a8dw4mteq44xufk` (`ma_tai_khoan`),
-  KEY `FK4kfboc7g5ory9apx9nntvvslw` (`ma_lop`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `nguoi_dung`
---
-
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `nguoi_tham_gia`
---
-
-
-CREATE TABLE IF NOT EXISTS `nguoi_tham_gia` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `id_buoihoc` int DEFAULT NULL,
-  `id_nguoidung` varchar(255) DEFAULT NULL,
-  `buoihoc_id` int DEFAULT NULL,
-  `nguoidung_id` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK87aqkpabi7bfukgu2jyh2bgcm` (`id_buoihoc`),
-  KEY `FK92kd0sjnal4olirs2r6mqgply` (`id_nguoidung`),
-  KEY `FKs7m9d0kal9qtv80wssa9lp6gj` (`buoihoc_id`),
-  KEY `FKtojp2r3lsbk1ssso6oca99og9` (`nguoidung_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `nguoi_tham_gia`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `phong`
---
-
-
 CREATE TABLE IF NOT EXISTS `phong` (
   `ma_phong` varchar(255) NOT NULL,
   `ten_phong` varchar(255) NOT NULL,
@@ -226,6 +131,117 @@ INSERT INTO `phong` (`ma_phong`, `ten_phong`) VALUES
 ('P015', 'Phòng C707'),
 ('P016', 'Phòng C708');
 
+CREATE TABLE IF NOT EXISTS `mon_hoc` (
+  `ma_mon_hoc` varchar(255) NOT NULL,
+  `ten_mon_hoc` varchar(255) NOT NULL,
+  PRIMARY KEY (`ma_mon_hoc`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `mon_hoc`
+--
+
+CREATE TABLE IF NOT EXISTS `nguoi_dung` (
+  `email` varchar(255) DEFAULT NULL,
+  `ma_lop` varchar(255) DEFAULT NULL,
+  `ma_nguoi_dung` varchar(255) NOT NULL,
+  `ma_tai_khoan` varchar(255) DEFAULT NULL,
+  `sdt` varchar(255) DEFAULT NULL,
+  `ten_nguoi_dung` varchar(255) NOT NULL,
+  PRIMARY KEY (`ma_nguoi_dung`),
+  UNIQUE KEY `UKmajqh5g4djy2tp3p9dvr64brp` (`email`),
+  UNIQUE KEY `UKltruy9naf2a8dw4mteq44xufk` (`ma_tai_khoan`),
+  KEY `FK4kfboc7g5ory9apx9nntvvslw` (`ma_lop`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `buoi_hoc` (
+  `ma_buoi_hoc` int NOT NULL AUTO_INCREMENT,
+  `ngay_hoc` date NOT NULL,
+  `tiet_bat_dau` int NOT NULL,
+  `tiet_ket_thuc` int NOT NULL,
+  `ma_mon_hoc` varchar(255) NOT NULL,
+  `ma_phong` varchar(255) NOT NULL,
+  `ma_tham_gia` varchar(255) DEFAULT NULL,
+  `gv_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ma_buoi_hoc`),
+  KEY `FKresnpp25ibokky9xcp7r9nn1e` (`ma_mon_hoc`),
+  KEY `FK1xu8i7qikw0dq270q1ejw8n27` (`ma_phong`),
+  CONSTRAINT `FKresnpp25ibokky9xcp7r9nn1e` FOREIGN KEY (`ma_mon_hoc`) REFERENCES `mon_hoc` (`ma_mon_hoc`),
+  CONSTRAINT `FK1xu8i7qikw0dq270q1ejw8n27` FOREIGN KEY (`ma_phong`) REFERENCES `phong` (`ma_phong`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `diem_danh` (
+  `ma_buoi_hoc` int NOT NULL,
+  `ma_diem_danh` int NOT NULL AUTO_INCREMENT,
+  `thoi_gian_diem_danh` datetime(6) DEFAULT NULL,
+  `ma_nguoi_dung` varchar(255) NOT NULL,
+  `phuong_thuc` enum('manual','qr_code') NOT NULL,
+  `trang_thai` enum('absent','late','present') NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `expired_at` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`ma_diem_danh`),
+  KEY `FKn39tnia8bkst1qbvkosdx8lew` (`ma_buoi_hoc`),
+  KEY `FK56ahhb3iy1e50gim96jbaxyku` (`ma_nguoi_dung`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoi_dung`
+--
+
+
+--
+-- Dumping data for table `nguoi_dung`
+--
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nguoi_tham_gia`
+--
+CREATE TABLE IF NOT EXISTS `diem_danh_sinh_vien` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `time` datetime(6) DEFAULT NULL,
+  `diemdanh_id` int DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKtflyllv7mahy2d1wxf18dncf9` (`diemdanh_id`),
+  KEY `FK5expuo5jtc2lkdw7n6wvngej8` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE IF NOT EXISTS `nguoi_tham_gia` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `id_buoihoc` int DEFAULT NULL,
+  `id_nguoidung` varchar(255) DEFAULT NULL,
+  `buoihoc_id` int DEFAULT NULL,
+  `nguoidung_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK87aqkpabi7bfukgu2jyh2bgcm` (`id_buoihoc`),
+  KEY `FK92kd0sjnal4olirs2r6mqgply` (`id_nguoidung`),
+  KEY `FKs7m9d0kal9qtv80wssa9lp6gj` (`buoihoc_id`),
+  KEY `FKtojp2r3lsbk1ssso6oca99og9` (`nguoidung_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nguoi_tham_gia`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phong`
+--
+
+
+
+
 -- --------------------------------------------------------
 
 --
@@ -233,20 +249,6 @@ INSERT INTO `phong` (`ma_phong`, `ten_phong`) VALUES
 --
 
 
-CREATE TABLE IF NOT EXISTS `tai_khoan` (
-  `ma_tai_khoan` varchar(255) NOT NULL,
-  `mat_khau` varchar(255) NOT NULL,
-  `ten_tai_khoan` varchar(255) NOT NULL,
-  `role` enum('admin','student','teacher') NOT NULL,
-  PRIMARY KEY (`ma_tai_khoan`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tai_khoan`
---
-
-INSERT INTO `tai_khoan` (`ma_tai_khoan`, `mat_khau`, `ten_tai_khoan`, `role`) VALUES
-('1234', '$2a$10$X.lhuj27aTwEUo9kNiokxexwIEjoVzaUkKD.RROLuSHcqDbuy6IHq', 'nguyenvana', 'admin')
 
 
 --
